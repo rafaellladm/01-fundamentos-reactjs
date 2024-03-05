34d42c334f79f968e8ccdf954a85bc0d889c9197
Rafael Luz <rafaellladm@gmail.com>
2024-03-04 22:09:46 -0300

    feat: Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilSimpleLine } from '@phosphor-icons/react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,12 +11,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/rafaellladm.png"/>
+
         <strong>Rafael Luz</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilSimpleLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
