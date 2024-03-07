547dec8afa6b61152f8f2c9a3b517cbc2baa1751
Rafael Luz <rafaellladm@gmail.com>
2024-03-07 18:05:13 -0300

    feat: Componente: Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/rafaellladm.png" />
+          <div className={styles.authorInfo}>
+            <strong>Rafael Luz</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="05 de Março ás 22:35" dateTime="2024-03-05 22:35:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a>jane.design/doctorcare</a></p>
+        <p><a>#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
