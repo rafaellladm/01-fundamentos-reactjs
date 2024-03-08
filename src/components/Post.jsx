e4b1f97ed3927b5ec74a3c28f91bbdc08d022362
Rafael Luz <rafaellladm@gmail.com>
2024-03-08 14:25:27 -0300

    feat: Estilizações do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,8 +18,12 @@ export function Post() {
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉 <a>jane.design/doctorcare</a></p>
-        <p><a>#novoprojeto #nlw #rocketseat</a></p>
+        <p>👉{'  '}<a>jane.design/doctorcare</a></p>
+        <p>
+          <a>#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
       </div>
     </article>
   )
