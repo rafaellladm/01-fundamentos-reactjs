e465fc07b2bd6c83f3fe3881df25c73002ec2940
Rafael Luz <rafaellladm@gmail.com>
2024-03-11 00:35:49 -0300

    feat: Component Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,6 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
 export function Post() {
@@ -6,7 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/rafaellladm.png" />
+          <Avatar src="https://github.com/rafaellladm.png" />
           <div className={styles.authorInfo}>
             <strong>Rafael Luz</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilSimpleLine } from '@phosphor-icons/react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/rafaellladm.png"/>
+        <Avatar src="https://github.com/rafaellladm.png" />
 
         <strong>Rafael Luz</strong>
         <span>Web Developer</span>
