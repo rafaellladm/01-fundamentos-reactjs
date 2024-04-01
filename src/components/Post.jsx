250b936639b8676fcce8e6767b71acb83530c3f9
Rafael Luz <rafael@Nitro5-Rafael>
2024-04-01 12:24:43 -0300

    feat: Validação de imput de formulario.
    Criado a necessidade do preenchimento do campo antes de submetê-lo.
    Criei estilo para o botão de submit.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,8 +30,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText('')
   }
 
+
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatiório!')
   }
 
   function deleteComment(commentToDelete) {
@@ -41,6 +47,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne)
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
     <article className={styles.post}>
@@ -75,10 +83,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder='Deixe um comentário'
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type='submit' disabled={isNewCommentEmpty} >
+            Publicar
+          </button>
         </footer>
       </form>
 
